41d136c57ee8de338656d0291a909d7b269da717
ssmith@domain.com <86253201+saraosmith@users.noreply.github.com>
2023-10-09 19:07:23 -0400

    updates

--- src/eclipseAndGithub/Driver.java
+++ src/eclipseAndGithub/Driver.java
@@ -7,6 +7,9 @@ public class Driver {
 		Glad.glad();
 		Hungry.hungry();
 		Sad.sad();
+		
 	}
+
+	
 
 }
--- src/eclipseAndGithub/Hungry.java
+++ src/eclipseAndGithub/Hungry.java
@@ -5,4 +5,13 @@ public class Hungry {
 	public static void hungry() {
 		System.out.println("We are very hungry"); 
 	}
+	
+		public static void veryHungry() {
+			System.out.println("we are still hungry");
+		}
+	
+	
 }
+
+
+
